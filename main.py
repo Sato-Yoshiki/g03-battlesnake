801e0a252ef2b22fe84cb82f37a958e0dbe76d0a
asamisea <sakakakan@gmail.com>
2023-10-24 13:24:03 +0900

    自分と衝突しないようにした。また首との衝突のプログラムをコメントアウトした。

--- main.py
+++ main.py
@@ -50,17 +50,17 @@ def move(game_state: typing.Dict) -> typing.Dict:
   my_head = game_state["you"]["body"][0]  # Coordinates of your head
   my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
 
-  if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
-    is_move_safe["left"] = False
+  # if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
+  #   is_move_safe["left"] = False
 
-  elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
-    is_move_safe["right"] = False
+  # elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
+  #   is_move_safe["right"] = False
 
-  elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
-    is_move_safe["down"] = False
+  # elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
+  #   is_move_safe["down"] = False
 
-  elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
-    is_move_safe["up"] = False
+  # elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
+  #   is_move_safe["up"] = False
 
   # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
   board_width = game_state['board']['width']
@@ -79,7 +79,24 @@ def move(game_state: typing.Dict) -> typing.Dict:
         is_move_safe["up"] = False
 
   # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-  # my_body = game_state['you']['body']
+  my_body = game_state['you']['body']
+  for i in range(1, len(my_body)):
+    dx = my_head["x"] - my_body[i]["x"]
+    dy = my_head["y"] - my_body[i]["y"]
+    
+    if dy == 0:
+      if dx == 1:
+        is_move_safe["left"] = False
+        continue
+      elif dx == -1:
+        is_move_safe["right"] = False
+        continue
+    elif dx == 0:
+      if dy == 1:
+        is_move_safe["down"] = False
+        continue
+      elif dy == -1:
+        is_move_safe["up"] = False
   
 
   # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
