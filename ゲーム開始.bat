50b526e714681489b489a572cf4409016294a5f3
piko <study.piko@gmail.com>
2023-12-18 00:23:43 +0900

    mainをコピー

---
+++ ゲーム開始.bat
@@ -0,0 +1,3 @@
+cd "battlesnake_CLI"
+battlesnake play -W 11 -H 11 --name 'Python Starter Project' --url http://localhost:8000 --url http://localhost:8000 -g duel --browser
+rem battlesnake play -W 11 -H 11 --name 'Python Starter Project' --url https://g03.pbl-b.repl.co --url https://g01.pbl-b.repl.co -g duel --browser
