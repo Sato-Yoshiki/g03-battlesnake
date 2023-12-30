d28b6dfc1e5c73dbde3ae8eaade1deff851ddf59
piko <study.piko@gmail.com>
2023-12-31 03:37:16 +0900

    WSL説明追加

---
+++ sh/2.sh
@@ -0,0 +1,2 @@
+cd ../battlesnake_Linux
+./battlesnake play -W 11 -H 11 --name 'Python Starter Project' --url http://localhost:8000 --url http://localhost:8000 -g duel --browser
--- ゲーム開始.bat
+++
@@ -1,6 +0,0 @@
-rem cd "battlesnake_CLI"
-rem battlesnake play -W 11 -H 11 --name 'Python Starter Project' --url http://localhost:8000 --url http://localhost:8000 -g duel --browser
-rem battlesnake play -W 11 -H 11 --name 'Python Starter Project' --url https://g03.pbl-b.repl.co --url https://g01.pbl-b.repl.co -g duel --browser
-
-
-pause
