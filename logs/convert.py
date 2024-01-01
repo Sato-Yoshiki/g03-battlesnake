c55f3c184a695844f88637ad1f63f75663aaa7d4
piko <study.piko@gmail.com>
2024-01-01 16:36:47 +0900

    修正

--- logs/convert.py
+++ logs/convert.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+from datetime import datetime
 
 
 def main():
@@ -18,6 +20,19 @@ def main():
         file.write("var gameLog = [\n")
         file.write(",\n".join(json_data_lines))
         file.write("\n];")
+
+    # ファイルをコピー
+    today_folder_name = datetime.now().strftime("%Y%m%d")
+    today_folder_path = os.path.join(script_dir, "history", today_folder_name)
+    if not os.path.exists(today_folder_path):
+        os.makedirs(today_folder_path)
+    file_index = int(len(os.listdir(today_folder_path)) / 2 + 1)
+    copy_log_filename = f"out{file_index}.log"
+    copy_js_filename = f"output{file_index}.js"
+    copy_log_path = os.path.join(today_folder_path, copy_log_filename)
+    shutil.copy2(log_file_path, copy_log_path)
+    copy_js_path = os.path.join(today_folder_path, copy_js_filename)
+    shutil.copy2(output_path, copy_js_path)
 
 
 if __name__ == "__main__":
