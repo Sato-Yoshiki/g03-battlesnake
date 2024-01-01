9a9af84fbc804bd031e2fa8ee12a8ea7fe6158b8
piko <study.piko@gmail.com>
2024-01-01 17:33:20 +0900

    修正

--- 2.sh
+++ 2.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-if [ ! -d "./logs/history/" ]; then
-  mkdir "./logs/history/"
-fi
 ./battlesnake_Linux/battlesnake play -W 11 -H 11 -n 'Python1' --url http://localhost:8000 -n 'Python2' --url http://localhost:8001 -o ./logs/history/out.log -v -g duel
 #./battlesnake_Linux/battlesnake play -W 11 -H 11 -n 'Python1' --url https://g03.pbl-b.repl.co -n 'Python2' --url https://g03.pbl-b.repl.co -g duel
